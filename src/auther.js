24f701d4392728d05ce427097693e52e8bd6fa49
HC Hacker <129988782+DevCHaali@users.noreply.github.com>
2024-05-05 07:29:19 -0400

    Add files via upload

---
+++ src/auther.js
@@ -0,0 +1,18 @@
+//Auther the Script ,not change !!!
+function auther(){
+  return "hamza"      
+}
+//you can change
+//function block for send message repeting
+function blockr(){
+  return "/block"      
+}
+//function block for send message repeting
+function pingr(){
+  return "/ping"      
+}
+//function block for send message repeting
+function helpr(){
+  return "/help"      
+}
+module.exports = { auther,blockr,pingr,helpr};
